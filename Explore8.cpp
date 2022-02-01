12ae7d401b1114f24d283cb9b7f98028861ecd9d
Keith <macdonak@loswego.k12.or.us>
2022-02-01 15:26:35 -0800

    Initial Version

---
+++ Explore8.cpp
@@ -0,0 +1,63 @@
+
+#include <iostream>
+using namespace std;
+struct Point {
+	double x;
+	double y;
+	Point();
+	Point(double xVal, double yVal);
+};
+struct Line {
+    Point start;
+    Point end;
+    Line(Point s, Point e);
+};
+Point::Point() {
+	x = 0;
+	y = 0;
+}
+
+Point::Point(double xVal, double yVal) {
+    x = xVal;
+    y = yVal;
+}
+
+Line::Line(Point s, Point e) {
+    start = s;
+    end = e;
+}
+
+void PrintPoint(Point p) {
+    cout << ": x = " << p.x << ", y = " << p.y;
+}
+
+
+void PrintLine(Line l) {
+    cout << "  Start";
+    PrintPoint(l.start);
+    cout << "   End";
+    PrintPoint(l.end); 
+    cout << endl;
+}
+
+// This is new
+Point FindMidPoint(Line l) {
+    Point mid;
+    mid.x = (l.start.x + l.end.x)/2.0;
+    mid.y = (l.start.y + l.end.y)/2.0;
+    return mid;
+}
+
+int main() {
+    Line a(Point(3,9), Point(12,22));
+    cout << "Line a";
+    PrintLine(a);
+    cout << "Line a's midpoint";
+    // This is new
+    Point midPoint = FindMidPoint(a);
+    PrintPoint(midPoint);
+    cout << endl;
+    return 0;
+}
+
+
